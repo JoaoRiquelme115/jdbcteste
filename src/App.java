df856fe4f778adbcef110c8837c913703c30dffc
Riquelme <132308905+JoaoRiquelme115@users.noreply.github.com>
2024-06-11 13:14:33 -0300

    Conexao

--- src/App.java
+++ src/App.java
@@ -1,6 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         
     }
+
+    public static Connection getConexao() {
+        Connection conexao = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://200.128.9.179:3306/bancoifba";
+            String username = "remoto";
+            String password = "remoto";
+            conexao = DriverManager.getConnection(url,username,password);
+            System.out.println("CONECTADO AO BANCO DE DADOS!");
+            return conexao;
+        } catch(ClassNotFoundException e) {
+            System.err.println("Não foi possível encontrar o Driver especificado!");
+            return null;
+        } catch(SQLException e) {
+            System.err.println("Não foi possível conectar ao Banco de Dados!");
+            return null;
+        }
+    }
+
 }
--- src/Conta.java
+++ src/Conta.java
@@ -1,3 +1,30 @@
 public class Conta {
-    
+    private String numero;
+    private Cliente cliente;
+    private Double saldo;
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Double getSaldo() {
+        return this.saldo;
+    }
+
 }
